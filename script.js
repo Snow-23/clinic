daf479a296873c84cfff5f6ea268f48ad4bcb77d
Snow-23 <74645525+Snow-23@users.noreply.github.com>
2022-02-09 20:06:33 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$(function() {
+  // 「#change-text」要素に対するclickイベントを作成してください
+  $('#change-text').click(function() {
+    // $('#text').text('いらっしゃいませ');
+    $('#change-text').slideUp(1);
+    $('img').fadeToggle(10000);
+  });
+  
+  // 「#change-html」要素に対するclickイベントを作成してください
+  $('#change-html').click(function(){
+  $('#text')
+  });
+  
+});
